8099bc1c10e651cc659011524e98f92e3f8c7930
rakesh-sirvi <87996971+rakesh-sirvi@users.noreply.github.com>
2021-07-30 19:23:34 +0530

    Update app.js

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ clear.addEventListener('click', () => {
 compute.addEventListener('click', () => {
   try{
     prev.innerText = curr.innerText + " = ";
-    curr.innerText = parseFloat(eval(curr.innerText.replace(/^0+/, '')),10);
+    curr.innerText = eval(curr.innerText.replace(/[^\d\.\-\+\*\/\(\)\e]/g, '').replace(/^0+/, '').replace(/[^\d\.]+0+/g,(s)=>s.substr(0, 1)));
   }catch(err){
     prev.innerText = 'Invalid expression'
   }
